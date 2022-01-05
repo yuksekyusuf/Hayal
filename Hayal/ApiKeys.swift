9ff678f60de43fd48ce17fabac9efef06685dfe2
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-01-05 00:11:19 -0500

    Add api keys

---
+++ Hayal/ApiKeys.swift
@@ -0,0 +1,14 @@
+//
+//  Keys.swift
+//  Hayal
+//
+//  Created by Ahmet Yusuf Yuksek on 1/5/22.
+//
+
+import Foundation
+
+class ApiKeys {
+    let key = ["key":"5379a3eda00bd01a30d2c95543a55e77",
+        "secret":"74e753fe68b0087f"
+    ]
+}
